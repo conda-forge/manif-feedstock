04ceebc7a4836144cd85cc3dbe10189de83d2720
Silvio Traversaro <silvio.traversaro@iit.it>
2021-07-22 18:18:44 +0200

    Update bld_py.bat

--- recipe/bld_py.bat
+++ recipe/bld_py.bat
@@ -1,3 +1,24 @@
-set CMAKE_GENERATOR=Visual Studio 15 2017
-python -m pip install . -vv
+:: Install the C++ related files in a random prefix so they are ignored,
+:: as they are already packaged by the manif packge in bld_cxx.bat
+:: Python bindings are installed outside CMAKE_INSTALL_PREFIX via the 
+:: DETECT_ACTIVE_PYTHON_SITEPACKAGES option
+cmake ^
+    -G "NMake Makefiles" ^
+    -DCMAKE_INSTALL_PREFIX=.\install ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DBUILD_TESTING=OFF ^
+    -DBUILD_EXAMPLES=OFF ^
+    -DUSE_SYSTEM_WIDE_TL_OPTIONAL=ON ^
+    -DBUILD_PYTHON_BINDINGS:BOOL=ON ^
+    -DDETECT_ACTIVE_PYTHON_SITEPACKAGES:BOOL=ON ^
+    %SRC_DIR%
 if errorlevel 1 exit 1
+
+:: Build.
+cmake --build . --config Release
+if errorlevel 1 exit 1
+
+:: Install.
+cmake --build . --config Release --target install
+if errorlevel 1 exit 1
+
