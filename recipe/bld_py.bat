f662ac46f7ae9b1cfc2aa9cc1451889b3ace5a75
Silvio Traversaro <silvio.traversaro@iit.it>
2021-07-23 08:15:29 +0200

    Update bld_py.bat

--- recipe/bld_py.bat
+++ recipe/bld_py.bat
@@ -2,6 +2,8 @@ set "CC=clang-cl.exe"
 set "CXX=clang-cl.exe"
 set "CL=/MP"
 
+set
+
 :: Install the C++ related files in a random prefix so they are ignored,
 :: as they are already packaged by the manif packge in bld_cxx.bat
 :: Python bindings are installed outside CMAKE_INSTALL_PREFIX via the 
@@ -14,9 +16,9 @@ cmake ^
     -DBUILD_EXAMPLES=OFF ^
     -DUSE_SYSTEM_WIDE_TL_OPTIONAL=ON ^
     -DBUILD_PYTHON_BINDINGS:BOOL=ON ^
-    -DPYTHON_EXECUTABLE:PATH=%PYTHON% ^
-    -DPython_EXECUTABLE:PATH=%PYTHON% ^
-    -DPython3_EXECUTABLE:PATH=%PYTHON% ^
+    -DPYTHON_EXECUTABLE:PATH="%PYTHON%" ^
+    -DPython_EXECUTABLE:PATH="%PYTHON%" ^
+    -DPython3_EXECUTABLE:PATH="%PYTHON%" ^
     -DDETECT_ACTIVE_PYTHON_SITEPACKAGES:BOOL=ON ^
     %SRC_DIR%
 if errorlevel 1 exit 1
