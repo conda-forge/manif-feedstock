dc88b2f94ce251bfb6729138f2226ec2f90a73c8
Silvio Traversaro <silvio@traversaro.it>
2021-07-26 19:07:54 +0200

    Enable build of manifpy on Windows (#13)
    Co-authored-by: conda-forge-linter <github-actions@email.com>

--- recipe/bld_py.bat
+++ recipe/bld_py.bat
@@ -1,2 +1,34 @@
-python -m pip install . -vv
+set "CC=clang-cl.exe"
+set "CXX=clang-cl.exe"
+set
+
+mkdir build%CONDA_PY%
+cd build%CONDA_PY%
+
+:: Install the C++ related files in a random prefix so they are ignored,
+:: as they are already packaged by the manif packge in bld_cxx.bat
+:: Python bindings are installed outside CMAKE_INSTALL_PREFIX via the 
+:: DETECT_ACTIVE_PYTHON_SITEPACKAGES option
+cmake ^
+    -G "NMake Makefiles" ^
+    -DCMAKE_INSTALL_PREFIX=.\install ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DBUILD_TESTING=OFF ^
+    -DBUILD_EXAMPLES=OFF ^
+    -DUSE_SYSTEM_WIDE_TL_OPTIONAL=ON ^
+    -DBUILD_PYTHON_BINDINGS:BOOL=ON ^
+    -DPython3_EXECUTABLE:PATH=%PYTHON% ^
+    -DPython3_LIBRARY:PATH=%PREFIX%\libs\python%CONDA_PY%.lib ^
+    -DDETECT_ACTIVE_PYTHON_SITEPACKAGES:BOOL=ON ^
+    %SRC_DIR%
+type CMakeCache.txt
 if errorlevel 1 exit 1
+
+:: Build.
+cmake --build . --config Release
+if errorlevel 1 exit 1
+
+:: Install.
+cmake --build . --config Release --target install
+if errorlevel 1 exit 1
+
