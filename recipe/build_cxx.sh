0b0d9cb341c2601179fb82145880188c5631d7f0
Silvio Traversaro <silvio@traversaro.it>
2022-06-29 18:33:36 +0200

    Update build_cxx.sh

--- recipe/build_cxx.sh
+++ recipe/build_cxx.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+if [[ ${target_platform} == "linux-ppc64le" || ${target_platform} == "linux-aarch64" ]]; then
+  echo "Using 1 thread to build"
+  export NUM_PARALLEL=-j1
+  export BUILD_EXAMPLES=OFF
+else
+  echo "Use all available cores to build"
+  export NUM_PARALLEL=
+  export BUILD_EXAMPLES=ON
+fi
+
 mkdir build
 cd build
 
@@ -8,10 +18,10 @@ cmake -GNinja .. \
       -DCMAKE_PREFIX_PATH=$PREFIX -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_INSTALL_LIBDIR=lib \
       -DBUILD_TESTING=OFF \
-      -DBUILD_EXAMPLES=ON \
+      -DBUILD_EXAMPLES=${BUILD_EXAMPLES} \
       -DUSE_SYSTEM_WIDE_TL_OPTIONAL=ON
 
-cmake --build . --config Release
+cmake --build . --config Release $NUM_PARALLEL
 cmake --build . --config Release --target install
 # Test suppressed as a workaround for https://github.com/conda-forge/staged-recipes/pull/14142#issuecomment-789047770
 # and https://github.com/conda-forge/manif-feedstock/pull/1#issuecomment-861226265
