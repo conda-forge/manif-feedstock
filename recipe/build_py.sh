13571c87f0dec49e2ebc0448364290ab8cb378db
Silvio Traversaro <silvio@traversaro.it>
2022-06-29 22:44:14 +0200

    Update build_py.sh

--- recipe/build_py.sh
+++ recipe/build_py.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-if [[ ${target_platform} == "linux-ppc64le" || ${target_platform} == "linux-aarch64" ]]; then
-    export CMAKE_BUILD_PARALLEL_LEVEL=1
-fi
-
 python -m pip install . -vv
